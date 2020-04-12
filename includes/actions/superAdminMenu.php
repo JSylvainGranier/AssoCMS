78275c61265b5b79cde360b8a80a8a750d05a5ef
hynemonth <jsylvain.granier@gmail.com>
2020-04-12 16:52:54 +0200

    Amélioration du menu d'administration

--- includes/actions/superAdminMenu.php
+++ includes/actions/superAdminMenu.php
@@ -4,6 +4,7 @@ if(!Roles::isSuperAdmin()){
 	throw new RoleException("Pas d'accès cette section si l'on n'est pas administrateur.");
 }
 
+$page->setTitle("Menu Administration");
 $page->appendBody(file_get_contents("includes/html/superAdminMenu.html"));
 
 
