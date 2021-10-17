18e792fd7f7756f469e010626282cb57c5ee5908
hynemonth <jsylvain.granier@gmail.com>
2021-10-17 12:35:46 +0200

    Faut que j'arrête d'utiliser Eclipse qui sais pas faire de l'autocomplettion avec PHP....

--- includes/actions/listInscriptionsVueGlobale.php
+++ includes/actions/listInscriptionsVueGlobale.php
@@ -1,15 +1,15 @@
 <?php 
 
-$page->append ( "Vue Globale" );
+$page->appendBody ( "Vue Globale" );
 
 if (! Roles::isGestionnaireCategorie()) {
 	header ( "HTTP/1.0 403 Forbidden" );
 	echo "403";
 	die ();
 }
-$page->append ( "Passe" );
+$page->appendBody ( "Passe" );
 $arr = Persistant::getDataFromQuery("select i.idInscription, i.etat, p.idPersonne, p.nom, p.prenom, prt.idProduit, prt.libelle from inscription i join personne p on p.idFamille = i.idFamille join inscription_personne_produit ipp on ipp.fkPersonne = p.idPersonne join produit prt on prt.idProduit = ipp.fkProduit where i.etat in (20,50) order by p.nom, p.prenom");
-$page->append ( "Query" );
-$page->append ( print_r($arr, true) );
-$page->append ( "Print" );
+$page->appendBody ( "Query" );
+$page->appendBody ( print_r($arr, true) );
+$page->appendBody ( "Print" );
 ?>
