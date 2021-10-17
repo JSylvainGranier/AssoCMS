98e0c02c3a3d89fa3b842139dca63be1e47ad684
hynemonth <jsylvain.granier@gmail.com>
2021-10-17 12:49:46 +0200

    4/500 Restitution de la synthèse des inscription

--- includes/actions/listInscriptionsVueGlobale.php
+++ includes/actions/listInscriptionsVueGlobale.php
@@ -7,9 +7,65 @@ if (! Roles::isGestionnaireCategorie()) {
 	echo "403";
 	die ();
 }
-$page->appendBody ( "Passe" );
-$arr = Persistant::getDataFromQuery("select i.idInscription, i.etat, p.idPersonne, p.nom, p.prenom, prt.idProduit, prt.libelle from inscription i join personne p on p.idFamille = i.idFamille join inscription_personne_produit ipp on ipp.fkPersonne = p.idPersonne join produit prt on prt.idProduit = ipp.fkProduit where i.etat in (20,50) order by p.nom, p.prenom");
-$page->appendBody ( "Query" );
-$page->appendBody ( print_r($arr, true) );
-$page->appendBody ( "Print" );
+
+$arr = Persistant::getDataFromQuery("SELECT i.idInscription, i.etat, p.idPersonne, p.nom, p.prenom, prt.idProduit, prt.libelle FROM personne p LEFT OUTER JOIN inscription i ON p.idFamille = i.idFamille LEFT OUTER JOIN inscription_personne_produit ipp ON ipp.fkPersonne = p.idPersonne LEFT OUTER JOIN produit prt ON prt.idProduit = ipp.fkProduit WHERE i.etat IS NULL  OR i.etat IN ( 20, 50 )  ORDER BY p.nom, p.prenom LIMIT 30 , 30");
+$tb = array();
+
+foreach($arr as $i => $rw){
+    $arrPersonne = $tb[$rw["idPersonne"]];
+    if($arrPersonne == null){
+        $arrPersonne = array();
+        $arrPersonne["idPersonne"] = $rw["idPersonne"];
+        $arrPersonne["nom"] = $rw["nom"];
+        $arrPersonne["prenom"] = $rw["prenom"];
+    }
+    if(isset($rw["idProduit"])){
+        $arrPersonne["p".$rw["idProduit"]] = true;
+    } else {
+        
+    }
+    
+    $tb[$rw["idPersonne"]] = $arrPersonne;
+}
+
+$p = new Produit();
+$tmpProds = $p->getAll();
+$prods = array();
+foreach($tmpProds as $ap){
+    $prods[$ap->idProduit] = $ap;
+}
+
+$pCells = "";
+
+foreach($prods as $ap){
+    $pCells .= "<td>{$ap->libelle}</td>";
+}
+
+$page->appendBody("<table>");
+$page->appendBody("<tr><td>Nom</td><td>Prénom</td>".$pCells."<td>Solde</td></tr>");
+
+foreach($tb as $i => $rw){
+    $pchecks = "";
+    foreach($prods as $ap){
+        $pchecks .= "<td>";
+        if($ap->idProduit == $rw["idProduit"]){
+            $pchecks .= "X";
+            //$ap["count"] += 1;
+        }
+        $pchecks .= "</td>";
+        
+    }
+    
+    $solde = 0.365;
+    
+    $page->appendBody ( "<tr><td>{$rw["nom"]}</td><td>{$rw["prenom"]}</td>{$pchecks}<td>{$solde}</td></tr>" );
+    
+}
+
+$page->appendBody("</table>");
+
+$page->appendBody("<pre>".print_r($tb, true)."</pre>");
+$page->appendBody("<pre>".print_r($arr)."</pre>");
+
+
 ?>
