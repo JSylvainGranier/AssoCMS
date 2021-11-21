3c6458deed888df7b79cda82bb1c675a509c9867
hynemonth <jsylvain.granier@gmail.com>
2021-11-21 19:59:30 +0100

    Bug inscriptions quantié libre lorsque l'on est en famille.

--- includes/actions/listInscriptionsVueGlobale.php
+++ includes/actions/listInscriptionsVueGlobale.php
@@ -9,7 +9,7 @@ if (! Roles::isGestionnaireCategorie()) {
 	die ();
 }
 
-$arr = Persistant::getDataFromQuery("SELECT i.idInscription, i.etat, ipp.quantite, p.idPersonne, p.idFamille, p.nom, p.prenom, p.email, p.telPortable, prt.idProduit, prt.libelle FROM personne p LEFT OUTER JOIN inscription i ON p.idFamille = i.idFamille LEFT OUTER JOIN inscription_personne_produit ipp ON ipp.fkInscription = i.idInscription LEFT OUTER JOIN produit prt ON prt.idProduit = ipp.fkProduit ORDER BY p.nom, p.prenom");
+$arr = Persistant::getDataFromQuery("SELECT i.idInscription, i.etat, ipp.quantite, p.idPersonne, p.idFamille, p.nom, p.prenom, p.email, p.telPortable, prt.idProduit, prt.libelle FROM personne p LEFT OUTER JOIN inscription i ON p.idFamille = i.idFamille LEFT OUTER JOIN inscription_personne_produit ipp ON ipp.fkInscription = i.idInscription  and ipp.fkPersonne = p.idPersonne  LEFT OUTER JOIN produit prt ON prt.idProduit = ipp.fkProduit ORDER BY p.nom, p.prenom");
 $tb = array();
 
 $regs = Persistant::getDataFromQuery("SELECT * from reglement where dateEcheance < now()");
