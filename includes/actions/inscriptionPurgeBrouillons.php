ad44fbaaddb7c2c5ec93e896a33376f6057817cc
hynemonth <jsylvain.granier@gmail.com>
2022-10-09 06:11:44 +0200

    Lien pour supprimer les brouillons

---
+++ includes/actions/inscriptionPurgeBrouillons.php
@@ -0,0 +1,15 @@
+<?php 
+
+if(!Roles::isSuperAdmin()){
+	throw new RoleException("Pas d'accès cette section si l'on n'est pas administrateur.");
+}
+
+$iscp = new Inscription();
+$iscp->clearOldBrouillons();
+$page->appendNotification ( "Purge des brouillons effectuée" );
+
+$ACTIONS [] = array (
+    "superAdminMenu"
+);
+
+?>
